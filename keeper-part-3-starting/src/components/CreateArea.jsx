5a15b6622e73a0ea6c1e5780a6e8ee23b0fa530a
srglbrkchn <75715053+srglbrkchn@users.noreply.github.com>
2021-12-09 11:40:26 +0100

    Use useState inorder to save state of input area.

--- keeper-part-3-starting/src/components/CreateArea.jsx
+++ keeper-part-3-starting/src/components/CreateArea.jsx
@@ -1,15 +1,44 @@
-import React from "react";
+import React, {useState} from "react";
 
 function CreateArea() {
-  return (
-    <div>
-      <form>
-        <input name="title" placeholder="Title" />
-        <textarea name="content" placeholder="Take a note..." rows="3" />
-        <button>Add</button>
-      </form>
-    </div>
-  );
+
+    const [inputTxt,
+        setInputTxt] = useState({title: "", content: ""});
+
+    function handleChange(event) {
+        const {name, value} = event.target;
+
+        setInputTxt((preValue) => {
+            return {
+                ...preValue,
+                [name]: value
+            }
+        });
+
+    }
+
+    function handleSubmit(event) {
+        event.preventDefault();
+    }
+
+    return (
+        <div>
+            <form onSubmit={handleSubmit}>
+                <input
+                    name="title"
+                    placeholder="Title"
+                    value={inputTxt.title}
+                    onChange={handleChange}/>
+                <textarea
+                    name="content"
+                    placeholder="Take a note..."
+                    rows="3"
+                    value={inputTxt.content}
+                    onChange={handleChange}/>
+                <button>Add</button>
+            </form>
+        </div>
+    );
 }
 
 export default CreateArea;
